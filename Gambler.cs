1144d87a3e96a93570ee0281ff44bee1e93d0cc9
dewanganshivam89@gmail.com <dewanganshivam89@gmail.com>
2020-01-22 18:11:06 +0530

    [Shivam]ADD.added a Gambler Program

---
+++ Gambler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicProgramming
+{
+    class Gambler
+    {
+        public static void gamble(int NoOfTimes, int Stake, int Goal)
+        {
+            int bets = 0;
+            int wins = 0;
+            Random r = new Random();
+
+            for (int i = 0; i < NoOfTimes; i++)
+            {
+                int cash = Stake;
+                int random = r.Next(0, 10);
+                while (cash > 0 && cash < Goal)
+                {
+                    bets++;
+                    if (random < 5)
+                        cash++;
+                    else
+                        cash--;
+                }
+                if (cash == Goal)
+                    wins++;
+            }
+            Console.WriteLine();
+            Console.WriteLine(wins + "Wins out of :" + NoOfTimes);
+            Console.WriteLine("Percentage of games won = " + 100 * wins / NoOfTimes);
+            Console.WriteLine("Total no of bets : " + NoOfTimes + " Games = " + bets);
+        }
+    }
+}
+    
+
