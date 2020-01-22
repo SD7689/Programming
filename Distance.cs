0de3f02bf9e19a9698c7caff7937d6a5821abbd0
dewanganshivam89@gmail.com <dewanganshivam89@gmail.com>
2020-01-22 18:07:16 +0530

    [Shivam]ADD.added a Euclidean distance of points Program

---
+++ Distance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicProgramming
+{
+    class Distance
+    {
+        public static void EuclideanDistance()
+        {
+            int x1 = 0, y1 = 0, x2 = 5, y2 = 5;
+            double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Sqrt(Math.Pow(y2 - y1, 2)));
+            Console.WriteLine("Euclidean Distance" + distance);
+        }
+    }
+}
