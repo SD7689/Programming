06cd6e6e98d0be8e2f5b91f0abe483626d6f3444
dewanganshivam89@gmail.com <dewanganshivam89@gmail.com>
2020-01-22 18:03:08 +0530

    [Shivam]ADD.added a 2D array Program

---
+++ Array2D.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicProgramming
+{
+    class Array2D
+    {
+        public static void IntegerArray(int row , int col)
+        {
+            int[,] arr = new int[row, col];
+
+            for(int i = 0; i< row; i++)
+            {
+                for(int j = 0; j < col; j++)
+                {
+                   
+                    arr[i, j] = Utility.readInt();
+
+                }
+            }
+            for(int i = 0; i < row; i++)
+            {
+                for(int j=0; j<col; j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine("");
+            }
+        }
+        /// <summary>
+        /// Double array print
+        /// </summary>
+        /// <param name="row"></param>
+        /// <param name="col"></param>
+        public static void DoubleArray(int row, int col)
+        {
+            int[,] arr = new int[row, col];
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    arr[i, j] = Utility.readInt();
+                }
+            }
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine("");
+            }
+        }
+        //Boolean array printing
+        public static void BooleanArray(int row, int col)
+        {
+
+            int[,] arr = new int[row, col];
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    arr[i, j] = Utility.readInt();
+                }
+            }
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
