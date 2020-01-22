adf1b20d32870731cb448281dc8be2c2033b0740
dewanganshivam89@gmail.com <dewanganshivam89@gmail.com>
2020-01-22 17:52:32 +0530

    [Shivam]ReplaceString.added a String Replace Program

---
+++ ReplaceString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicProgramming
+{
+    class ReplaceString
+    {
+        public static void Replace()
+        {
+            string name = "Hello Username , Welcome to .net Programming!!";
+            Console.WriteLine("Enter a name to be replaced");
+            string replaceName = Utility.readString();
+            string replace = "";
+            if (replaceName.Length < 3)
+                Console.WriteLine("Enter a Valid String of More than 3 Character:");
+            else
+                replace = name.Replace("Username", replaceName);
+            Console.WriteLine(replace);
+        }
+             
+
+    }
+}
