8fe271901de78a403532749adaa2b8352d30cd29
dewanganshivam89@gmail.com <dewanganshivam89@gmail.com>
2020-01-22 18:12:22 +0530

    [Shivam]ADD.added a Random number generator(Coupon) Program

---
+++ CouponNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicProgramming
+{
+    class CouponNumber
+    {
+        public static void couponNumber()
+        {
+			/*
+	 * char array to get characters for coupen
+	 */
+			char[] chars = "abcdefghijklmnop".ToCharArray();
+			/*
+			 * length of coupen
+			 */
+			int max = 100000000;
+			/*
+			 * random value
+			 */
+			Random random1 = new Random();
+			double d = random1.NextDouble();
+			int random = (int)(d * max);
+			String sb = "";
+			Console.WriteLine("random double " + d + " random at start " + random);
+
+			while (random > 0)
+			{
+				sb = sb + (chars[random % chars.Length]);
+				random /= chars.Length;
+				Console.WriteLine(random + "  " + random % chars.Length + " sb is " + sb);
+			}
+
+			String couponCode = sb.ToString();
+			Console.WriteLine(value: "Coupon Code: " + couponCode);
+		}
+	}
+}
