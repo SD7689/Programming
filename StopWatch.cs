20a70b6e0638de0cb6902eb402d27b5aaae0d56b
dewanganshivam89@gmail.com <dewanganshivam89@gmail.com>
2020-01-22 18:13:26 +0530

    [Shivam]ADD.added a StopWatch (ElapsedTime) Program

---
+++ StopWatch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+
+namespace BasicProgramming
+{
+    class StopWatch
+    {
+        public static void StopMain()
+        {
+            // Create new stopwatch.
+            Stopwatch stopwatch = new Stopwatch();
+
+            // Begin timing.
+            stopwatch.Start();
+
+            // Do something.
+            for (int i = 0; i < 1000; i++)
+            {
+                Thread.Sleep(1);
+            }
+
+            // Stop timing.
+            stopwatch.Stop();
+
+            // Write result.
+            Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+        }
+    }
+}
+    
+
