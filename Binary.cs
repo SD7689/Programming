65cb3162a5a74481157f2f81055859f96aef8931
raghul-jegan <raghuljegan@gmail.com>
2020-01-28 18:51:55 +0530

    [Shivam]ADD.added a Decimal to binary program

---
+++ Binary.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicProgramming.JUnitTesting
+{
+    class Binary
+    {
+        public static void DecToBinary(int n)
+        {
+            int[] binaryNum = new int[32];
+            int i = 0;
+            while(n > 0)
+            {
+                binaryNum[i] = n % 2;
+                n = n / 2;
+                i++;
+            }
+            for(int j = i-1; j>=0; j--)
+            {
+                Console.Write(binaryNum[j]);
+            }
+        }
+    }
+}
