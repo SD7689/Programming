ef5d36f9fbf94afa21f425a6b4214c7358c638d8
raghul-jegan <raghuljegan@gmail.com>
2020-01-28 18:50:02 +0530

    [Shivam]ADD.added a Monthly Payment program

---
+++ MonthlyPayment.cs
@@ -0,0 +1,29 @@
+﻿using ProgrammingCsharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicProgramming.JUnitTesting
+{
+    class MonthlyPayment
+    {
+        public static void Payment()
+        {
+            Console.WriteLine("Enter loan Amount:");
+            double P = Utility.readDouble();
+            Console.WriteLine("Enter a year :");
+            double Y = Utility.readDouble();
+            Console.WriteLine("Enter Intrest Rate:");
+            double R = Utility.readDouble();
+
+            double n = 12 * Y;
+            double r = R / (12 * 100);
+
+            double payments = (P * r) / (1 - Math.Pow(1 + r, -n));
+            double interest = payments * n - P;
+
+            Console.WriteLine("Monthly payment is: " +payments);
+            Console.WriteLine("Intrest rate is: " + interest);
+        }
+    }
+}
