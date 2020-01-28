f9956a9e663a87df5badb79a8e2c914a3521f39c
raghul-jegan <raghuljegan@gmail.com>
2020-01-28 16:44:59 +0530

    [Shivam]Add.A utility class to read int, double, string,boolean values

---
+++ ProgrammingCsharp/Utility.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingCsharp
+{
+
+    class Utility
+    {
+        public static int readInt()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+        public static String readString()
+        {
+            String s = Console.ReadLine();
+            return s;
+        }
+        public static double readDouble()
+        {
+            return Convert.ToDouble(Console.ReadLine());
+        }
+        public static bool readBool()
+        {
+            return Convert.ToBoolean(Console.ReadLine());
+        }
+    }
+}
