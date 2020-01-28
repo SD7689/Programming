97385ebf69500894c4f447d79dc77700c0f0dffe
raghul-jegan <raghuljegan@gmail.com>
2020-01-28 18:52:34 +0530

    [Shivam]ADD.added a Swap nibble program

---
+++ BinarySwaapNibble.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicProgramming.JUnitTesting
+{
+    class BinarySwaapNibble
+    {
+        public static int SwapNibble(int x)
+        {
+            return ((x & 0x0F) << 4 |
+            (x & 0xF0) >> 4);
+        }
+    }
+}
