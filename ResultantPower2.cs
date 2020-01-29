f89667634d843fe8cbd8ac2dd3f5d28658dd4729
SD7689 <dewanganshivam89@gmail.com>
2020-01-29 10:04:40 +0530

    [Shivam]ADD.Added a Resultant no is power of 2 program

---
+++ ResultantPower2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingCsharp.JUnitTesting
+{
+    class ResultantPower2
+    {
+        public static bool isPowerOfTwo(int n)
+        {
+
+            if (n == 0)
+                return false;
+
+            while (n != 1)
+            {
+                if (n % 2 != 0)
+                    return false;
+
+                n = n / 2;
+            }
+            return true;
+        }
+
+    }
+}
