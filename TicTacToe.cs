2cbd2ace38e8527a6861bdc600187fa5b4ce645a
SD7689 <dewanganshivam89@gmail.com>
2020-01-30 16:55:23 +0530

    [Shivam]Add.added a comment in program

--- TicTacToe.cs
+++ TicTacToe.cs
@@ -1,6 +1,6 @@
 // --------------------------------------------------------------------------------------------------------------------
 // <copyright file="TictaeToe.cs" company="Bridgelabz">
-//   Copyright © 2019 Company="BridgeLabz"
+//   Copyright © 2020 Company="BridgeLabz"
 // </copyright>
 // <creator name="Shivam Dewangan"/>
 // --------------------------------------------------------------------------------------------------------------------
@@ -8,7 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace BasicProgramming
+namespace Programmings
 {
 	class TicTacToe
 	{
