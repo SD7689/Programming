9cb6c26a70b9deedf4d127e7a7396aa5b6df96a1
SD7689 <dewanganshivam89@gmail.com>
2020-01-30 17:56:40 +0530

    [Shivam]Add.added a Harmonic no program

---
+++ Harmonic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programmings
+{
+    class Harmonic
+    {
+        public static void harmonic(int N)
+        {
+            double i, h = 0;
+            for (i = 1; i <= N; i++)
+               h = h + 1 / i;
+
+            Console.WriteLine("HArmonic no is : " + h);
+
+
+        }
+
+
+    }
+}
