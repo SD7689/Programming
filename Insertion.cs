bbd18413f355c266b9f4e288fa6d860acd90aaac
SD7689 <dewanganshivam89@gmail.com>
2020-01-30 17:46:43 +0530

    [Shivam]Add.added a Insertion sort program

---
+++ Insertion.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=Insertion.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Shivam Dewangan"/>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programmings
+{
+    class Insertion
+    {
+        public static void InsertionSort()
+        {
+            int[] arr = new int[10] { 23, 9, 56, 86, 78, 45, 3, 45, 32, 23 };
+            int n = 10, j, i, val, flag;
+            Console.WriteLine("Insertion Sort -");
+            Console.WriteLine("Intial Array is: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine(arr[i] + "");
+            }
+            for (i = 1; i < n; i++)
+            {
+                val = arr[i];
+                flag = 0;
+                for (j = i - 1; j >= 0 && flag != 1;)
+                {
+                    if (val < arr[j])
+                    {
+                        arr[j + 1] = arr[j];
+                        j--;
+                        arr[j + 1] = val;
+                    }
+                    else
+                    {
+                        flag = 1;
+                    }
+                }
+                Console.WriteLine(" Sorted array is :");
+                for (i = 0; i < n; i++)
+                {
+                    Console.WriteLine(arr[i] + "");
+                }
+            }
+        }
+    }
+}
