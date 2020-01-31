07382d0ea6522ff1b83a62c57b411db31a7d2050
SD7689 <59974361+SD7689@users.noreply.github.com>
2020-01-31 13:10:54 +0530

    [Shivam]Add.added a Iterative permutation Method

--- permutationIterative.cs
+++ permutationIterative.cs
@@ -1,44 +1,48 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 // <copyright file=PermutationIterative.cs" company="Bridgelabz">
 //   Copyright © 2020 Company="BridgeLabz"
 // </copyright>
 // <creator name="Shivam Dewangan"/>
 // 
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Programmings
 {
     class permutationIterative
     {
-        public static String swapString(String a, int i, int j)
+        public static void RotateRight(IList sequence, int count)
         {
-            char[] b = a.ToCharArray();
-            char ch;
-            ch = b[i];
-            b[i] = b[j];
-            b[j] = ch;
-            //Converting characters from array into single string  
-            return string.Join("", b);
+            object tmp = sequence[count - 1];
+            sequence.RemoveAt(count - 1);
+            sequence.Insert(0, tmp);
         }
-        public static void generatePermutation(String str, int start, int end)
+
+        public static IEnumerable<IList> Permutate(IList sequence, int count)
         {
-            //Prints the permutations  
-            if (start == end - 1)
-                Console.WriteLine(str);
+            if (count == 1) yield return sequence;
             else
             {
-                for (int i = start; i < end; i++)
+                for (int i = 0; i < count; i++)
                 {
-                    //Swapping the string by fixing a character  
-                    str = swapString(str, start, i);
-                    //Recursively calling function generatePermutation() for rest of the characters   
-                    generatePermutation(str, start + 1, end);
-                    //Backtracking and swapping the characters again.  
-                    str = swapString(str, start, i);
+                    foreach (var perm in Permutate(sequence, count - 1))
+                        yield return perm;
+                    RotateRight(sequence, count);
                 }
             }
+        }
+        public static void PermutateStringTest()
+        {
+            string a = "word";
+            foreach (List<char> perm in Permutate(a.ToCharArray().ToList(), a.Length))
+            {
+                string s = new string(perm.ToArray());
+                Console.Write(s + "\t");
+            }
+            Console.WriteLine();
         }
     }
 }
