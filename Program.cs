812495fd32eff62625a0f2c0c0ed1086837c38c4
SD7689 <dewanganshivam89@gmail.com>
2020-02-03 10:29:48 +0530

    [Shivam]ADD.added a main method Program

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
-// --------------------------------------------------------------------------------------------------------------------
+﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file=Programming.cs" company="Bridgelabz">
 //   Copyright © 2020 Company="BridgeLabz"
 // </copyright>
 // <creator name="Shivam Dewangan"/>
 // --------------------------------------------------------------------------------------------------------------------
 
+using BasicProgramming;
 using System;
 
 namespace Programmings
@@ -32,31 +33,23 @@ namespace Programmings
                     break;
 
                 case 3:
-                    LeapYear.leapYear();
+                    LeapYear.LeapYears();
                     break;
 
                 case 4:
-                    PowerOf2.printTable();
+                    PowerOf2.PrintTable();
                     break;
 
-                case 5:
-                    Console.WriteLine("Enter a No.");
-                    int N = Utility.readInt();
-                    Harmonic.harmonic(N);
+                case 5:                  
+                    Harmonic.HarmonicNo();
                     break;
 
-                case 6:
-                    Console.WriteLine("Enter a No.");
-                    int n = Utility.readInt();
-                    PrimeFactor.Primefactor(n);
+                case 6:                    
+                    PrimeFactor.Primefactor();
                     break;
 
                 case 7:
-                    Console.WriteLine("Enter Temp in fahrenheit:");
-                    double f = Utility.readDouble();
-                    Console.WriteLine("Enter wind speed in miles per Hour:");
-                    double ws = Utility.readDouble();
-                    WindChill.Windchill(f, ws);
+                    WindChill.Chill();
                     break;
 
                 case 8:
@@ -68,29 +61,15 @@ namespace Programmings
                     break;
 
                 case 10:
-                    int[] arr = { 0, -1, 2, -3, 1 };
-                    n = arr.Length;
-                    Sumof3No.FindTriplet(arr, n);
+                    Sumof3No.SumZero();
                     break;
 
-                case 11:
-                    Console.WriteLine("Enter No of times to Gamble:");
-                    int NoOfTimes = Utility.readInt();
-                    Console.WriteLine("Enter The Amount The Gamble Have:");
-                    int Stake = Utility.readInt();
-                    Console.WriteLine("Enter The desired Goal:");
-                    int Goal = Utility.readInt();
-                    Gambler.gamble(NoOfTimes, Stake, Goal);
+                case 11:                   
+                    Gambler.Gambling();
                     break;
 
                 case 12:
-                    Console.WriteLine("\n  Integer 2D array");
-                    Console.WriteLine("Enter no. of rows:");
-                    int row1 = Utility.readInt();
-                    Console.WriteLine("Enter no. of coloms:");
-                    int col1 = Utility.readInt();
-                    Console.WriteLine("Enter " + row1 * col1 + "values.");
-                    Array2D.IntegerArray(row1, col1);
+                    Array2D.IntegerArray();
                     break;
 
                 case 13:
@@ -98,16 +77,15 @@ namespace Programmings
                     break;
 
                 case 14:
-                    StopWatch.StopMain();
+                    StopWatch.Watch();
                     break;
 
                 case 15:
-                    TicTacToe.play();
+                    TicTacToe.TictaeToeGame();
                     break;
 
                 case 16:
-                    int amount = Utility.readInt();
-                    VendingMachine.countCurrency(amount);
+                    VendingMachine.Atm();
                     break;
 
                 case 17:
@@ -119,33 +97,27 @@ namespace Programmings
                     break;
 
                 case 19:
-                    MonthlyPayment.Payment();
+                    MonthlyPayment.PaymentMonthly();
                     break;
 
                 case 20:
                     Sqrt.Root();
                     break;
 
-                case 21:
-                    Console.WriteLine("Enter a Number to Convert: ");
-                    n = Utility.readInt();
-                    Binary.DecToBinary(n);
+                case 21:                  
+                    Binary.DecToBinary();
                     break;
 
-                case 22:
-                    int x = 100;
-                    BinarySwaapNibble.SwapNibble(x);
+                case 22:             
+                    BinarySwaapNibble.SwapNibble();
                     break;
 
                 case 23:
-                    Console.WriteLine("Enter a number: ");
-                    n = Utility.readInt();
-                    Console.WriteLine("Resultant no is Power of two :");
-                    Console.WriteLine(ResultantPower2.isPowerOfTwo(n) ? "Yes" : "No");
+                    ResultantPower2.Resultant();
                     break;
 
                 case 24:
-
+                    BinarySearch.Search();
                     break;
 
                 case 25:
@@ -153,25 +125,11 @@ namespace Programmings
                     break;
 
                 case 26:
-                    Insertion.InsertionSort();
+                    Insertion.InsertionSorting();
                     break;
 
                 case 27:
-                    int[] arreee = { 76, 89, 23, 1, 55, 78, 99, 12, 65, 100 };
-                    n = 10;
-                    int i = 0;
-                    Console.WriteLine("Merge Sort");
-                    Console.Write("Initial array is: ");
-                    for (i = 0; i < n; i++)
-                    {
-                        Console.Write(arreee[i] + " ");
-                    }
-                    MergeSort.mergeSort(arreee, 0, n - 1);
-                    Console.Write("\nSorted Array is: ");
-                    for (i = 0; i < n; i++)
-                    {
-                        Console.Write(arreee[i] + " ");
-                    }
+                    MergeSort.Mergesorting();
                     break;
 
                 case 28:
@@ -183,19 +141,21 @@ namespace Programmings
                     break;
 
                 case 30:
-                    String str = "ABC";
-                    int z = str.Length;
-                    Permutation.permute(str, 0, z - 1);
+                    Permutation.Recursion();
                     break;
 
                 case 31:
-                    String str2 = "DEF";
-                    int len = str2.Length;
-                    Console.WriteLine("All the permutations of the string are: ");
-                    permutationIterative.generatePermutation(str2, 0, len);
+                    permutationIterative.Iterative();
+                    break;
+
+                case 32:
+                    PlaindromePrimeNo.AnagramPrime();
+                    //PlaindromePrimeNo.Palindrome();
                     break;
             }
         }
+
+
     }
 }
 
